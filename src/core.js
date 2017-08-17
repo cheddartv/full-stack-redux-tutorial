eeccf8bfe6072a7983f3365e0a3f1f782aa4875a
Julia Castro <castro.morales.julia@gmail.com>
2017-08-17 17:27:00 -0400

    in the middle of implementing winning function

--- src/core.js
+++ src/core.js
@@ -4,6 +4,14 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries))
 }
 
+function getWinners(vote) {
+  if (!vote) return [];
+
+  const [a, b] = vote.get('pair')
+  const aVotes = vote.getIn(['tally', a], 0)
+  const bVotes = vote.getIn(['tally', b], 0)
+}
+
 export function next(state) {
   const entries = state.get('entries')
   return state.merge({
