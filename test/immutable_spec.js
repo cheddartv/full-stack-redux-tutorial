bb413d336dcc1768f1539d19ed525e72878320df
Julia Castro <castro.morales.julia@gmail.com>
2017-08-17 15:05:07 -0400

    test bug fix

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -38,7 +38,7 @@ describe('immutability', () => {
 
   describe('a tree', () => {
     function addMovie(currentState, movie) {
-      return currentState.update('update', movies => movies.push(movie))
+      return currentState.update('movies', movies => movies.push(movie))
     }
 
     it('is immutable', () => {
